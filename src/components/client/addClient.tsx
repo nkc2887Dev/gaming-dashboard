31309803eda554b8c2268357bf77eaf26f295e5b
nkc2887dev <nkc2887dev@gmail.com>
2025-02-27 22:30:34 +0530

    git pull

--- src/components/client/addClient.tsx
+++ src/components/client/addClient.tsx
@@ -1,6 +1,16 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import Input from "@/components/ui/input";
 import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { ChevronDown } from "lucide-react";
+import { useState } from "react";
 
 type ClientFormData = {
   userType: string;
@@ -16,7 +26,17 @@ type ClientFormData = {
   downlineSharing: string;
   maxBet: string;
   maxProfit: string;
+  masterPassword: string;
 };
+
+const ROLES = [
+  "Select User Type",
+  "Admin",
+  "Super Master",
+  "Sub Super Master",
+  "Master",
+  "User",
+];
 
 const ClientFormDefaultData = {
   userType: undefined,
@@ -32,6 +52,7 @@ const ClientFormDefaultData = {
   downlineSharing: undefined,
   maxBet: undefined,
   maxProfit: undefined,
+  masterPassword: undefined,
 };
 
 const AddClient = ({
@@ -39,6 +60,8 @@ const AddClient = ({
 }: {
   setPage: (page: "clientList" | "addClient") => void;
 }) => {
+  const [selectedUserType, setSelectedUserType] = useState(ROLES[0]);
+
   const {
     register,
     handleSubmit,
@@ -65,14 +88,27 @@ const AddClient = ({
               <CardContent className="flex items-center gap-3 p-5">
                 <div className="w-full">
                   <label className="text-black text-sm">Account Type:</label>
-                  <Input
-                    type="text"
-                    {...register("userType", {
-                      required: "Account Type is required",
-                    })}
-                    className="text-sm h-9 px-4 py-2 w-full bg-white border border-gray-400 text-gray rounded-md pr-10 focus:outline-none"
-                    placeholder="Select user type"
-                  />
+                  <Select
+                    value={selectedUserType}
+                    onChange={setSelectedUserType}
+                    className="w-full"
+                  >
+                    <SelectTrigger className="flex items-center justify-between text-sm h-9 px-4 py-2 bg-white border border-gray-400 text-gray rounded-md focus:outline-none">
+                      <SelectValue placeholder={selectedUserType} />
+                      <ChevronDown className="h-4 w-4 ml-2" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {ROLES.map((role) => (
+                        <SelectItem
+                          key={role}
+                          value={role}
+                          className="rounded-md hover:bg-[#3F83F8] py-1 hover:text-white"
+                        >
+                          {role}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
                   {errors.userType && (
                     <p className="text-red-500 text-xs">
                       {errors.userType.message}
@@ -169,8 +205,8 @@ const AddClient = ({
               <div className="bg-white absolute -top-3 left-4 px-2 text-sm font-bold">
                 Commission (Cricket):
               </div>
-              <CardContent className="flex p-5">
-                <div className="flex items-center gap-2 w-full justify-center flex-wrap">
+              <CardContent className="flex pt-5">
+                <div className="flex items-center space-x-1 w-full justify-center flex-wrap">
                   <span className="font-bold text-sm whitespace-nowrap">
                     Commission: M.O
                   </span>
@@ -179,7 +215,7 @@ const AddClient = ({
                     {...register("commission", {
                       required: "Commission is required",
                     })}
-                    className="text-sm h-9 w-2/3 bg-white border border-gray-400 text-gray-900 rounded-md focus:outline-none"
+                    className="text-sm w-auto bg-white border border-gray-400 text-gray-900 rounded-md focus:outline-none"
                   />
                   <span className="font-bold text-sm whitespace-nowrap">
                     % (1%)
@@ -244,15 +280,85 @@ const AddClient = ({
                 </div>
               </CardContent>
             </Card>
+            <Card className="relative border border-black">
+              <div className="bg-white absolute -top-3 left-4 px-2 text-sm font-bold">
+                Sport & Casino Balance
+              </div>
+              <CardContent className="flex items-center gap-3 p-5">
+                <div className="w-full">
+                  <label className="text-black text-sm">
+                    Credit Reference:
+                  </label>
+                  <Input
+                    type="text"
+                    {...register("creditReference", {
+                      required: "Credit Reference is required",
+                    })}
+                    className="text-sm h-9 px-4 py-2 w-full bg-white border border-gray-400 text-gray rounded-md pr-10 focus:outline-none"
+                    placeholder="Credit Reference"
+                  />
+                  {errors.creditReference && (
+                    <p className="text-red-500 text-xs">
+                      {errors.creditReference.message}
+                    </p>
+                  )}
+                </div>
+                <div className="w-full">
+                  <label className="text-black text-sm">Add Deposit:</label>
+                  <Input
+                    type="number"
+                    {...register("addDeposit", {
+                      required: "Add Deposit is required",
+                    })}
+                    className="text-sm h-9 px-4 py-2 w-full bg-white border border-gray-400 text-gray rounded-md pr-10 focus:outline-none"
+                    placeholder="Amount"
+                  />
+                  {errors.addDeposit && (
+                    <p className="text-red-500 text-xs">
+                      {errors.addDeposit.message}
+                    </p>
+                  )}
+                </div>
+                <div className="w-full">
+                  <label className="text-black text-sm">Deposit Remark:</label>
+                  <Input
+                    type="text"
+                    {...register("depositRemark", {
+                      required: "Deposit Remark is required",
+                    })}
+                    className="text-sm h-9 px-4 py-2 w-full bg-white border border-gray-400 text-gray rounded-md pr-10 focus:outline-none"
+                    placeholder="Remark"
+                  />
+                  {errors.depositRemark && (
+                    <p className="text-red-500 text-xs">
+                      {errors.depositRemark.message}
+                    </p>
+                  )}
+                </div>
+              </CardContent>
+            </Card>
           </div>
           {/* Submit Button */}
           <div className="mt-4">
-            <button
-              type="submit"
-              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
-            >
-              Submit
-            </button>
+            <label className="text-black text-sm">Master Password:</label>
+            <div className="flex items-center gap-2">
+              <div>
+                <Input
+                  type="text"
+                  {...register("masterPassword", {
+                    required: "Master Password is required",
+                  })}
+                  className="text-sm h-9 px-4 py-2 w-full bg-white border border-gray-400 text-gray rounded-md pr-10 focus:outline-none"
+                />
+              </div>
+              <Button
+                type="submit"
+                onClick={() => setPage("addClient")}
+                className="w-auto text-nowrap rounded-sm text-sm font-bold border border-[1.5px] border-[#026473] bg-gradient-to-b from-[#2E899C] to-[#159ab3] text-white shadow-md hover:from-[#159ab3] hover:to-[#03364c]"
+              >
+                Create Account
+              </Button>
+            </div>
           </div>
         </form>
       </div>
--- src/components/ui/popup.tsx
+++ src/components/ui/popup.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import { X } from "lucide-react";
-import blackPanther from "@/assets/image/blackpanther.jpg";
+import ADMIN_splashpopup from "@/assets/image/ADMIN_splashpopup.jpg";
 
 export default function Popup() {
   const [isVisible, setIsVisible] = useState(false);
@@ -23,20 +23,25 @@ export default function Popup() {
   if (!isVisible) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="relative rounded-lg shadow-lg">
+    <div
+      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
+      aria-hidden={!isVisible}
+    >
+      <div className="relative rounded-lg shadow-lg max-w-md mx-auto">
         <button
-          className="absolute top-2 right-2 text-gray-600 hover:text-gray-900"
+          className="absolute top-3 right-3 text-gray-600 hover:text-gray-900"
           onClick={handleClose}
+          aria-label="Close popup"
         >
           <X className="w-6 h-6 text-white" />
         </button>
         <Image
-          src={blackPanther}
+          src={ADMIN_splashpopup}
           alt="Promo"
           width={1000}
           height={200}
           className="rounded-md"
+          priority
         />
       </div>
     </div>
