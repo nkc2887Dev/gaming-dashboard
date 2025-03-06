edaab612ae44bfd48d8da506e4ea246c9cca6d96
nkc2887dev <nkc2887dev@gmail.com>
2025-03-06 16:21:59 +0530

    optimise

--- src/components/scrolling-footer.tsx
+++ src/components/scrolling-footer.tsx
@@ -1,26 +1,16 @@
 "use client";
 
-import { useEffect, useState } from "react";
-// import { LucideMegaphone } from "lucide-react";
+import { LucideMegaphone } from "lucide-react";
 import styles from "@/styles/SlidingFooter.module.css";
 
 const SlidingFooter = ({ messages }: { messages: string[] }) => {
-  const [isAnimating, setIsAnimating] = useState<boolean>(false);
-
-  useEffect(() => {
-    setIsAnimating(true);
-  }, []);
-
   return (
     <div className="bg-gradient-to-b from-[#16A3BB] to-[#03364c] w-full">
       <div className="relative flex items-center w-full overflow-hidden">
-        <div
-          className={`text-white mt-1 ${styles.text} ${isAnimating ? styles.animate : ""}`}
-        >
+        <div className={`text-white mt-1 ${styles.text} ${styles.animate}`}>
           {messages.map((message, idx) => (
             <div key={idx} className="inline-flex items-center gap-1 text-sm">
-              {/* <LucideMegaphone /> */}
-              <span>📢</span>
+              <LucideMegaphone />
               <span>{message}</span>
             </div>
           ))}
